ffb53013b7248946985c5a475627cabb822f50cb
cody-and <codya.0807@icloud.com>
2023-08-15 17:40:25 -0500

    finished routes and moved server file outside of the public folder

--- Develop/public/assets/js/apiRoutes.js
+++ Develop/public/assets/js/apiRoutes.js
@@ -1,27 +1,45 @@
-let notes = [
-  { id: 1, title: 'Note 1', text: 'This is note 1.' },
-  { id: 2, title: 'Note 2', text: 'This is note 2.' },
-];
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+const readNotesFromFile = () => {
+  const data = fs.readFileSync('./db.json', 'utf8');
+  return JSON.parse(data);
+};
+
+const writeNotesToFile = (notes) => {
+  fs.writeFileSync('./db.json', JSON.stringify(notes), 'utf8');
+};
 
 const getNotes = (req, res) => {
+  const notes = readNotesFromFile();
   res.json(notes);
 };
 
-let nextNoteId = 1;
-
 const saveNote = (req, res) => {
-  const newNote = {
-    id: nextNoteId++,
-    title: req.body.title,
-    text: req.body.text,
-  };
-  notes.push(newNote);
-  res.json(newNote);
+  try {
+    const newNote = {
+      id: uuidv4(),
+      title: req.body.title,
+      text: req.body.text,
+    };
+    
+    const notes = readNotesFromFile();
+    notes.push(newNote);
+    
+    writeNotesToFile(notes);
+    
+    res.json(newNote);
+  } catch (error) {
+    console.error('Error saving note:', error);
+    res.status(500).json({ error: 'Failed to save note.' });
+  }
 };
 
 const deleteNote = (req, res) => {
   const noteId = req.params.id;
-  // Delete note logic here
+  let notes = readNotesFromFile();
+  notes = notes.filter((note) => note.id !== noteId);
+  writeNotesToFile(notes);
   res.json({ message: 'Note deleted successfully!' });
 };
 
--- Develop/server.js
+++ Develop/server.js
@@ -3,13 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-const { getNotes, saveNote, deleteNote } = require('./apiRoutes');
+const { getNotes, saveNote, deleteNote } = require('./public/assets/js/apiRoutes');
 
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/api/notes', getNotes);
 app.post('/api/notes', saveNote);
